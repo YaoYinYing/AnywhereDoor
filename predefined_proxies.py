c3754e41e4e1ba80e1e0ee81d4de26b4002188e0
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2024-05-13 13:04:21 +0800

    proxies: drop shell clash

--- predefined_proxies.py
+++ predefined_proxies.py
@@ -4,35 +4,37 @@ from dataclasses import dataclass
 
 # todo: add parser for config file: `.anywheredoorrc`?
 predefined_proxies: List[ProxyConfig] = [
+    # ProxyConfig(
+    #     "z4.login2.japs.yaoyy.moe",
+    #     "10089",
+    #     "10089",
+    #     "oreouser",
+    #     "ling7412",
+    #     alternative_urls=(
+    #         "ts.login2.japs.yaoyy.moe",
+    #         "z4.login3.japs.yaoyy.moe",
+    #         "ts.login3.japs.yaoyy.moe",
+    #     ),
+    # ),
     ProxyConfig(
-        "z4.login2.japs.yaoyy.moe",
-        "10089",
-        "10089",
-        "oreouser",
-        "ling7412",
+        "localhost"
+        "10091",
+        "10092",
         alternative_urls=(
+            "z4.login1.japs.yaoyy.moe",
+            "ts.login1.japs.yaoyy.moe",
+            "z4.login2.japs.yaoyy.moe",
             "ts.login2.japs.yaoyy.moe",
             "z4.login3.japs.yaoyy.moe",
             "ts.login3.japs.yaoyy.moe",
         ),
     ),
-    ProxyConfig(
-        "z4.login2.japs.yaoyy.moe",
-        "10091",
-        "10092",
-        alternative_urls=(
-            "ts.login2.japs.yaoyy.moe",
-            "z4.login1.japs.yaoyy.moe",
-            "ts.login1.japs.yaoyy.moe",
-            "localhost"
-        ),
-    ),
-    ProxyConfig(
-        "z4.login1.japs.yaoyy.moe",
-        "10089",
-        "10089",
-        alternative_urls=("ts.login1.japs.yaoyy.moe",),
-    ),
+    # ProxyConfig(
+    #     "z4.login1.japs.yaoyy.moe",
+    #     "10089",
+    #     "10089",
+    #     alternative_urls=("ts.login1.japs.yaoyy.moe",),
+    # ),
 ]
 
 
